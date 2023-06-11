fbf08010c248e76eac84fa28b7ac4e80e69a65b5
AhmadHasbyBik <abik262002@gmail.com>
2023-06-11 19:20:16 +0700

    fix

--- src/pages/addCrossings.js
+++ src/pages/addCrossings.js
@@ -1,68 +1,66 @@
 import axios from "axios";
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { Form, Button } from "react-bootstrap";
+import React from "react";
+import { Button, Form } from "react-bootstrap";
+import { v4 as uuidv4 } from 'uuid';
 
-export default function AddCrossings() {
-  const [name, setName] = useState("");
-  const [latitude, setLatitude] = useState("");
-  const [longitude, setLongitude] = useState("");
-  const [heading, setHeading] = useState("");
-
-  const navigate = useNavigate();
-  const data = {
-    name: name,
-    latitude: latitude,
-    longitude: longitude,
-    heading: heading
+export default function AddCrossings({ handleClose, handleDataUpdate }) {
+  const submitForm = (e) => {
+    e.preventDefault();
+    const id = uuidv4();
+    const data = {
+      id: id,
+      name: e.target.name.value,
+      latitude: e.target.latitude.value,
+      longitude: e.target.longitude.value,
+      heading: e.target.heading.value
+    };
+    axios.post("http://localhost:5000/api", data)
+      .then(() => {
+        // Call the handleDataUpdate function passed as a prop
+        handleDataUpdate();
+        // Reset form fields
+        e.target.reset();
+      })
+      .catch((error) => {
+        console.log(error);
+        // Handle error
+      });
   };
 
-  function submitForm(e) {
-    e.preventDefault();
-    axios.post("http://localhost:5000/api", data).then(navigate("/crossings"));
-  }
   return (
     <Form onSubmit={submitForm}>
       <Form.Group>
-          <Form.Control
-              type="text"
-              placeholder="Name"
-              name="name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              required
-          />
+        <Form.Control
+          type="text"
+          name="name"
+          placeholder="Name"
+          required
+        />
       </Form.Group><br></br>
       <Form.Group>
-          <Form.Control
-              type="text"
-              placeholder="Latitude"
-              name="latitude"
-              value={latitude}
-              onChange={(e) => setLatitude(e.target.value)}
-          />
+        <Form.Control
+          type="text"
+          name="latitude"
+          placeholder="Latitude"
+        />
       </Form.Group><br></br>
       <Form.Group>
-          <Form.Control
-              type="text"
-              placeholder="Longitude"
-              name="longitude"
-              value={longitude}
-              onChange={(e) => setLongitude(e.target.value)}
-          />
+        <Form.Control
+          type="text"
+          name="longitude"
+          placeholder="Longitude"
+        />
       </Form.Group><br></br>
       <Form.Group>
-          <Form.Control
-              type="text"
-              placeholder="Header"
-              name="heading"
-              value={heading}
-              onChange={(e) => setHeading(e.target.value)}
-          />
+        <Form.Control
+          type="text"
+          name="heading"
+          placeholder="Heading"
+        />
       </Form.Group><br></br>
       <Button variant="success" type="submit" block>
-          Create
+        Create
       </Button>
-  </Form>
-  )
-};
+    </Form>
+  );
+}
